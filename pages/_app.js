2a1cec0b67936108405a514894373b13b652f818
Wynston Hou <4262443+wynsto@users.noreply.github.com>
2023-10-18 10:41:15 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>

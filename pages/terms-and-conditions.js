6f59f9a4abb7c222509589dbf06e58051a377c25
Wynston Hou <4262443+wynsto@users.noreply.github.com>
2023-10-18 11:17:11 +0800

    terms and conditions

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import { getPosts } from '../utils/mdx-utils';
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout, { GradientBackground } from '../components/Layout';
+import ArrowIcon from '../components/ArrowIcon';
+import { getGlobalData } from '../utils/global-data';
+import SEO from '../components/SEO';
+
+export default function Index({ posts, globalData }) {
+  return (
+    <Layout>
+      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <Header name={globalData.name} />
+      <main className="w-full">
+        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+          {globalData.blogTitle}
+        </h1>
+        <p>Terms and conditions</p>
+      </main>
+      <Footer copyrightText={globalData.footerText} />
+      <GradientBackground
+        variant="large"
+        className="fixed top-20 opacity-40 dark:opacity-60"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
+  );
+}
+
+export function getStaticProps() {
+  const posts = getPosts();
+  const globalData = getGlobalData();
+
+  return { props: { posts, globalData } };
+}
